9c1b0bb9da69360580931c32401896e75618c5bf
m0zh1 <hersi@duck.com>
2022-07-01 06:57:30 -0700

    Added C program that prints given string using printf Fixed: betty linting issues on 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * Output: "Programming is like building a multilingual puzzle" using puts
- * return 0 if exited without err, otherwise return non-zero
+ * main - output "Programming is like building a multilingual puzzle" using puts
+ * return: 0 if exited without err, otherwise return non-zero
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints given phrase with proper grammar but the outcome is a,
+ * piece of art.
+ * RiReturn: 0 if exit correct, otherwise, return non-zero
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
