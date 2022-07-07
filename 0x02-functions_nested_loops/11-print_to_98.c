95fc091e6320a7b875aa3dbd42ab726c25561cb2
m0zh1 <hersi@duck.com>
2022-07-06 21:56:56 -0700

    Bug fix on file 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,28 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n <= 98)
+	if (n > 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (n >= 98)
 		{
-			if (i != 98)
-				printf("%d", i);
-			else if (i == 98)
-				printf("%d\n", i);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n -= 1;
 		}
 	}
-	else if (n >= 98)
+	else
 	{
-		for (j = n; n >= 98; j--)
+		while (n <= 98)
 		{
-			if (j != 98)
-				printf("%d", j);
-			else if (j == 98)
-				printf("%d\n", j);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n += 1;
 		}
 	}
 }
