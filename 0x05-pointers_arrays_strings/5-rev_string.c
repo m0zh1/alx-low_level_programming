616d2284b8029601bcf60021ebb4a21b94a3e0c2
m0zh1 <hersi@duck.com>
2022-07-11 12:25:27 -0700

    Added file 5-rev-string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int length(char *str);
+
+/**
+ * rev_string - reverses a string
+ * @s: param of type char *
+ */
+void rev_string(char *s)
+{
+	int len, idx;
+	char *start, *end, temp;
+
+	len = length(s);
+	start = s;
+	end = s;
+
+	for (idx = 0; idx < len - 1; idx++)
+		end++;
+	for (idx = 0; idx < len / 2; idx++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+
+		start++;
+		end++;
+	}
+}
+
+/**
+ * length - returns the length of a string
+ * @str: param of type char *
+ * Return: length of @str
+ */
+int length(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != '\0')
+		count++;
+
+	return (count);
+}
