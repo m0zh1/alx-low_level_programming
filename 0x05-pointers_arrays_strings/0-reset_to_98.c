44ba0d760d12a28539c6039d8972ee5b9410923b
m0zh1 <hersi@duck.com>
2022-07-11 10:47:12 -0700

    Initial commit of project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - updates the pointer parameter
+ * @n:m int type pointer
+ * Return: always 0
+ */
+int main(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Pointers101
+File reset_to_98 updates the value of a param of type int * to 98.
+
+
