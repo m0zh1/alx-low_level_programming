5c320819b152290ab6c188b839d232e929103527
m0zh1 <hersi@duck.com>
2022-07-12 12:12:06 -0700

    Added file 101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * passwd_gen - computes valid random passwords
+ * Return: 0
+ */
+void passwd_gen(void)
+{
+	int randm, ch;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+
+	while (ch < 2772)
+	{
+		randm = rand() % 128;
+		if ((ch + randm) > 2772)
+			break;
+		ch += randm;
+		printf("%c", randm);
+	}
+	printf("%c\n", (2772 - ch));
+	return (0);
+}
+
+/**
+ * main - entry0
+ * Return: always 0
+ */
+int main(void)
+{
+	passwd_gen();
+}
