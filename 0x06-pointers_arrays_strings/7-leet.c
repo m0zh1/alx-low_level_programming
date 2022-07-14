5f4916c6bb3d2885b967a78f750920211f33fda0
m0zh1 <hersi@duck.com>
2022-07-14 12:20:01 -0700

    added file 7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encode a string to 1337
+ * @c: param of type char *, input string
+ * Return: encoded string
+ */
+char *leet(char *c)
+{
+	int lowercase[] = {97, 101, 108, 111, 116};
+	int uppercase[] = {65, 69, 76, 79, 84};
+	int nums[] = {48, 49, 51, 52, 55};
+	int idx, i;
+
+	idx = 0;
+
+	while (c[idx] != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (c[idx] == lowercase[i] || c[idx] == uppercase[i])
+			{
+				c[idx] = nums[i];
+				break;
+			}
+		}
+		idx++;
+	}
+	return (c);
+}
