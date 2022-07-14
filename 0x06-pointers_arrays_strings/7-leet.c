513c4cf5415ba7fb7abc50aa137996e382b828ab
m0zh1 <hersi@duck.com>
2022-07-14 12:33:39 -0700

    fix bug

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,9 +7,9 @@
  */
 char *leet(char *c)
 {
-	int lowercase[] = {97, 101, 108, 111, 116};
-	int uppercase[] = {65, 69, 76, 79, 84};
-	int nums[] = {48, 49, 51, 52, 55};
+	int lowercase[] = "aeotl";
+	int uppercase[] = "AEOTL";
+	int nums[] = "43071";
 	int idx, i;
 
 	idx = 0;
