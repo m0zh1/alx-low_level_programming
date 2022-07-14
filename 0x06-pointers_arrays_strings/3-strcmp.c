dbea98b3247b868ee41512540b7f1deec89a70c6
m0zh1 <hersi@duck.com>
2022-07-14 13:27:48 -0700

    fixed bug in 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,26 +8,32 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int op_status = 0;
+	int idx, op_stat;
 
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
+	idx = 0;
+	op_stat = 0;
 
-	if (*s1 < *s2)
+	while (true)
 	{
-		op_status = -1;
+		if (s1[idx] == '\0' && s2[idx] == '\0')
+			break;
+		else if (s1[idx] == '\0')
+		{
+			op_stat = s2[idx];
+			break;
+		}
+		else if (s2[idx] == '\0')
+		{
+			op_stat = s1[idx];
+			break;
+		}
+		else if (s1[idx] != s2[idx])
+		{
+			op_stat = s1[idx] - s2[idx];
+			break;
+		}
+		else
+			idx++;
 	}
-	else if (*s1 == *s2)
-	{
-		op_status = 0;
-	}
-	else
-	{
-		op_status = 1;
-	}
-
-	return (op_status);
+	return (op_stat);
 }
