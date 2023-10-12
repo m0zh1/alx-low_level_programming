b135ff9e352975fe74b1b585da282d4e181caa75
m0zh1 <hersi@duck.com>
2023-10-12 16:13:01 +0300

    bug fix to print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,10 +7,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i <= 7; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (int j = 0; j <= 7; j++)
+		{
 			_putchar(a[i][j]);
+		}
 		_putchar('\n');
 	}
 }
